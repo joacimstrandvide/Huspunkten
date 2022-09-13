5565f3086045a0e58e699c70dcbe615718deab81
Joacim <18618554+Majoritycactus9@users.noreply.github.com>
2022-09-13 11:05:24 +0200

    Update script.js

--- script.js
+++ script.js
@@ -15,3 +15,21 @@ toggleButton.addEventListener('click', () => {
   navbarLinks.classList.toggle('active')
 
 })
+
+//Slideshow
+let slideIndex = 0;
+showSlides();
+
+function showSlides() {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {
+    slideIndex = 1;
+  }
+  slides[slideIndex - 1].style.display = "block";
+  setTimeout(showSlides, 6000); // Change image every 2 seconds
+}
